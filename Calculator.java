385ab2444565a07822ce8f29930b95775916b255
Iryna_Kvasnytsya <ikvasnytsya@vertafore.com>
2022-04-14 20:31:32 +0300

    Add implementation for add operation for 3 param

--- Calculator.java
+++ Calculator.java
@@ -4,4 +4,9 @@ public class Calculator {
         System.out.println("This is add method");
         return a + b;
     }
+
+    public int add(int a, int b, int c) {
+        System.out.println("This is add method for 3 params");
+        return a + b + c;
+    }
 }
